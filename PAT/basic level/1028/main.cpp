4b7b42fbc05b78b99d4590d131585afebcbe8696
Frostmurne <1599101385@qq.com>
2020-08-31 20:02:57 +0800

    有个小Bug，不知道哪错了

---
+++ PAT/basic level/1028/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+struct credit{
+    string name;
+    string birthday;
+};
+bool comparision(const credit& a, const credit& b){
+    return a.birthday<b.birthday;
+}
+int main() {
+    int n;
+    cin >> n;
+    auto *arr = new credit[n];
+    for(int i=0;i<n;++i){
+        cin >> arr[i].name;
+        cin >> arr[i].birthday;
+    }
+    sort(arr,arr+n, comparision);
+    int pre=0;
+    int post = n-1;
+    while(pre<post&&arr[pre].birthday<"1814/09/06")
+        ++pre;
+    while(post>pre&&arr[post].birthday>"2014/09/06")
+        --post;
+    cout << post-pre+1 << ' ' << arr[pre].name << ' ' <<  arr[post].name;
+    return 0;
+}
