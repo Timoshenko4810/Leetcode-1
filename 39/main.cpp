ffc6b66264f0bb0867b6190da049b5f9ec4b802a
Timoshenko <1853839@tongji.edu.cn>
2020-09-09 11:19:32 +0800

    39

---
+++ 39/main.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+	vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+		vector<vector<int>> res;
+		if(candidates.size() == 0) return res;
+		vector<int> vec;
+		for(int i = 0; i < candidates.size(); ++i){
+			if(candidates[i] <= target){
+				vec.push_back(candidates[i]);
+				dfs(res, candidates, vec, i, target, candidates[i]);
+				vec.pop_back();
+			}
+		}
+		return res;
+	}
+
+	void dfs(vector<vector<int>>& res, vector<int>& candidates, vector<int>& vec, int i, int target, int sum){
+		if(sum == target) {res.push_back(vec); return;}
+		for(int j = i; j < candidates.size(); ++j){
+			if((sum + candidates[j]) <= target){
+				vec.push_back(candidates[j]);
+				dfs(res, candidates, vec, j, target, sum + candidates[j]);
+				vec.pop_back();
+			}
+		}
+	}
+};
